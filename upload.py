030239ad49426a65734ae718ab182a8368b0fa23
Alexander Swensen <alex.swensen@gmail.com>
2019-01-03 13:13:55 -0500

    Add script for uploading

--- upload.py
+++ upload.py
@@ -2,6 +2,8 @@ import boto3
 import os
 import threading
 import sys
+from datetime import datetime
+from pathlib import Path
 
 class ProgressPercentage(object):
     def __init__(self, filename):
@@ -27,6 +29,30 @@ space_name = 'raspbian-store'
 key_id = os.environ.get('boto_id')
 key_secret = os.environ.get('boto_secret')
 
+# Generate filenames for uploading
+today = datetime.today()
+file_date = today.strftime('%Y-%m-%d') # 2019-01-03
+file_base_name = f'image_{file_date}-Raspbian'
+lite_file_name = f'{file_base_name}-lite.zip'
+norm_file_name = f'{file_base_name}.zip'
+full_file_name = f'{file_base_name}-full.zip'
+
+# check that all files exist
+
+files = []
+
+for file_name in [lite_file_name, norm_file_name, full_file_name]:
+    file_path = f'{base_path}{file_name}'
+    file = Path(file_path)
+
+    # if the file exists
+    if Path(file_path).is_file():
+        files.append({
+            'name': file_name,
+            'path': file_path
+            }) # add it to the path
+    else:
+        raise Exception(f'{file_path} doesn\'t exist')
 
 session = boto3.session.Session()
 client = session.client('s3',
@@ -35,9 +61,13 @@ client = session.client('s3',
                         aws_access_key_id=key_id,
                         aws_secret_access_key=key_secret)
 
-client.upload_file('./deploy/image_2018-12-31-Raspbian-lite.zip',  # Path to local file
-                   space_name,  # Name of Space
-                   'image_2018-12-31-Raspbian-lite.zip', # Name for remote file
-                   Callback=ProgressPercentage("./deploy/image_2018-12-31-Raspbian-lite.zip"))
-
-
+for file in files:
+    file_path = file.get('path', "")
+    file_name = file.get('name', "")
+    print(f"Uploading {file_name}...")
+    client.upload_file(
+        file_path,
+        space_name,
+        file_name,
+        Callback=ProgressPercentage(file_path)
+    )
